9d129c72608af67377cdb04505d132577b05e453
hglasswater-boop <h.glasswater@gmail.com>
2026-09-03 11:23:59 +0900

    refactor: use Compose view model integration

--- app/src/main/java/app/clipforge/MainActivity.kt
+++ app/src/main/java/app/clipforge/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.lifecycle.viewmodel.compose.viewModel
 import app.clipforge.ui.ClipForgeApp
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { ClipForgeApp(viewModel) }
+        setContent {
+            val mainViewModel: MainViewModel = viewModel()
+            ClipForgeApp(mainViewModel)
+        }
     }
 }
