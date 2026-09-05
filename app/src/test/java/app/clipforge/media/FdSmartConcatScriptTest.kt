56122a30b910e40a31045571bbe1373caeb8b3fe
hglasswater-boop <h.glasswater@gmail.com>
2026-09-05 21:14:46 +0900

    Test smart concat descriptor edge cases

--- app/src/test/java/app/clipforge/media/FdSmartConcatScriptTest.kt
+++ app/src/test/java/app/clipforge/media/FdSmartConcatScriptTest.kt
@@ -30,4 +30,34 @@ outpoint 3.000
             script,
         )
     }
+
+    @Test
+    fun allowsNoDescriptorsWhenEveryPartIsRendered() {
+        val script = fdSmartConcatScript(
+            fds = emptyList(),
+            parts = listOf(
+                SmartConcatInput.RenderedFile("/tmp/start.mkv"),
+                SmartConcatInput.RenderedFile("/tmp/end.mkv"),
+            ),
+        )
+
+        assertEquals(
+            """ffconcat version 1.0
+file '/tmp/start.mkv'
+file '/tmp/end.mkv'
+""",
+            script,
+        )
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun rejectsDescriptorCountThatDoesNotMatchSourceParts() {
+        fdSmartConcatScript(
+            fds = listOf(51),
+            parts = listOf(
+                SmartConcatInput.SourceSegment(MediaSegment(0L, 1_000L)),
+                SmartConcatInput.SourceSegment(MediaSegment(2_000L, 3_000L)),
+            ),
+        )
+    }
 }
