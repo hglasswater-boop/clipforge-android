01764fdb86d568a96e0e591451c6d22adf969588
hglasswater-boop <h.glasswater@gmail.com>
2026-09-06 18:30:54 +0900

    Update progress tests for sparse pipeline order

--- app/src/test/java/app/clipforge/media/AutoTrimProgressTest.kt
+++ app/src/test/java/app/clipforge/media/AutoTrimProgressTest.kt
@@ -13,7 +13,7 @@ class AutoTrimProgressTest {
         val tracker = AutoTrimProgressTracker { now }
 
         now = 1_000L
-        val early = tracker.update(AutoTrimPhase.START_SCENE, 0.1)
+        val early = tracker.update(AutoTrimPhase.START_AUDIO, 0.1)
 
         assertNull(early.remainingMs)
     }
@@ -24,7 +24,7 @@ class AutoTrimProgressTest {
         val tracker = AutoTrimProgressTracker { now }
 
         now = 2_000L
-        val progress = tracker.update(AutoTrimPhase.START_SCENE, 0.5)
+        val progress = tracker.update(AutoTrimPhase.START_AUDIO, 0.5)
 
         assertEquals(10, progress.overallPercent)
         assertEquals(50, progress.phasePercent)
@@ -40,7 +40,7 @@ class AutoTrimProgressTest {
         now = 2_000L
         val laterPhase = tracker.update(AutoTrimPhase.END_AUDIO, 0.7)
         now = 3_000L
-        val staleUpdate = tracker.update(AutoTrimPhase.START_SCENE, 0.2)
+        val staleUpdate = tracker.update(AutoTrimPhase.START_AUDIO, 0.2)
 
         assertTrue(laterPhase.overallPercent > 0)
         assertEquals(laterPhase.overallPercent, staleUpdate.overallPercent)
